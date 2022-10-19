5243d0e3f71d59f1085ac436e9f98c70a29f46d9
Spatriciopk <spatriciopk@gamil.com>
2022-10-18 23:56:57 -0500

    Juego de la calculadora listo

--- app/src/main/java/com/example/my_app2/MainActivity2.java
+++ app/src/main/java/com/example/my_app2/MainActivity2.java
@@ -3,12 +3,74 @@ package com.example.my_app2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private TextView et1,et2,et3,et5;
+    private EditText et4;
+    private TextView tv;
+    private double int_num1,int_num2;
+    private int int_num3;
+    private String operacion []={"+","-","*","/"};
+    private Button calcular;
+    private  DecimalFormat df;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+         df = new DecimalFormat("#.00");
+        et1 = (TextView) findViewById(R.id.double_num1);
+        et2 = (TextView) findViewById(R.id.double_num2);
+        et3 = (TextView) findViewById(R.id.str_operacion);
+        int_num1 =  Double.parseDouble(df.format((Math.random()*10+1)));
+        int_num2 =  Double.parseDouble(df.format((Math.random()*10+1)));
+        int_num3 = (int) (Math.random()*4);
+        et1.setText(String.valueOf(int_num1));
+        et2.setText(String.valueOf(int_num2));
+        et3.setText(String.valueOf(operacion[int_num3]));
+        calcular= (Button) findViewById(R.id.btn_resultadoJuego);
     }
+
+    public void verificar(View vista){
+        tv = (TextView) findViewById(R.id.str_resultado_juego);
+        et4 = (EditText) findViewById(R.id.double_respuesta);
+        et5 = (TextView) findViewById(R.id.str_resultado_juego);
+        double result = Double.parseDouble(et4.getText().toString());
+        double resultado_num = 0;
+        if( operacion[int_num3].equals("+")){
+            resultado_num = Double.parseDouble(df.format(int_num1+int_num2));
+            if(result == resultado_num)
+                et5.setText("Felicitaciones");
+
+            else
+                et5.setText("Vuelva a Intentarlo");
+        }
+        else if ( operacion[int_num3].equals("-")){
+            resultado_num = Double.parseDouble(df.format(int_num1-int_num2));
+            if(result == resultado_num)
+                et5.setText("Felicitaciones");
+            else
+                et5.setText("Vuelva a Intentarlo");
+        }
+        else if ( operacion[int_num3].equals("*")){
+            resultado_num = Double.parseDouble(df.format(int_num1*int_num2));
+            if(result == resultado_num)
+                et5.setText("Felicitaciones");
+            else
+                et5.setText("Vuelva a Intentarlo");
+        }
+        else if ( operacion[int_num3].equals("/")){
+            resultado_num = Double.parseDouble(df.format(int_num1/int_num2));
+            if(result == resultado_num)
+                et5.setText("Felicitaciones");
+            else
+                et5.setText("Vuelva a Intentarlo");
+        }
+    }
+
 }
