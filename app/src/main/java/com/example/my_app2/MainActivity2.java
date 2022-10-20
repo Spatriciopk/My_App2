0546771998c1a4ae2b2436ad9f96d0703a116396
Spatriciopk <spatriciopk@gmail.com>
2022-10-20 02:07:56 +0200

    Implementacion de imagenes y reload

--- app/src/main/java/com/example/my_app2/MainActivity2.java
+++ app/src/main/java/com/example/my_app2/MainActivity2.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -17,25 +18,42 @@ public class MainActivity2 extends AppCompatActivity {
     private double int_num1,int_num2;
     private int int_num3;
     private String operacion []={"+","-","*","/"};
+    private String url [] = {"suma.jpg","resta.jpg","multi.jpg","division.png"};
     private Button calcular;
     private  DecimalFormat df;
+    private ImageView image,imggan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-         df = new DecimalFormat("#.00");
+        df = new DecimalFormat("#.00");
         et1 = (TextView) findViewById(R.id.double_num1);
         et2 = (TextView) findViewById(R.id.double_num2);
-        et3 = (TextView) findViewById(R.id.str_operacion);
-        int_num1 =  Double.parseDouble(df.format((Math.random()*10+1)));
-        int_num2 =  Double.parseDouble(df.format((Math.random()*10+1)));
-        int_num3 = (int) (Math.random()*4);
-        et1.setText(String.valueOf(int_num1));
-        et2.setText(String.valueOf(int_num2));
-        et3.setText(String.valueOf(operacion[int_num3]));
+        //et3 = (TextView) findViewById(R.id.str_operacion);
+        reload();
         calcular= (Button) findViewById(R.id.btn_resultadoJuego);
     }
 
+    public void reload(){
+        int_num1 =  Double.parseDouble(df.format((Math.random()*10+1)));
+        int_num2 =  Double.parseDouble(df.format((Math.random()*10+1)));
+        int_num3 = (int) (Math.random()*4);
+        image = (ImageView) findViewById(R.id.imageView3);
+        imggan = (ImageView) findViewById(R.id.gano);
+        imagen();
+        et1.setText(String.valueOf(int_num1));
+        et2.setText(String.valueOf(int_num2));
+    }
+    public void imagen(){
+        if(int_num3 == 0)
+            image.setImageResource(R.drawable.suma);
+        else if(int_num3==1)
+            image.setImageResource(R.drawable.resta);
+        else if (int_num3==2)
+            image.setImageResource(R.drawable.multi);
+        else if (int_num3 ==3)
+            image.setImageResource(R.drawable.division);
+    }
     public void verificar(View vista){
         tv = (TextView) findViewById(R.id.str_resultado_juego);
         et4 = (EditText) findViewById(R.id.double_respuesta);
@@ -44,32 +62,57 @@ public class MainActivity2 extends AppCompatActivity {
         double resultado_num = 0;
         if( operacion[int_num3].equals("+")){
             resultado_num = Double.parseDouble(df.format(int_num1+int_num2));
-            if(result == resultado_num)
+            if(result == resultado_num){
                 et5.setText("Felicitaciones");
+                imggan.setImageResource(R.drawable.correcto);
+                reload();
+            }
 
-            else
+            else{
+                imggan.setImageResource(R.drawable.error);
                 et5.setText("Vuelva a Intentarlo");
+            }
+
         }
         else if ( operacion[int_num3].equals("-")){
             resultado_num = Double.parseDouble(df.format(int_num1-int_num2));
-            if(result == resultado_num)
+            if(result == resultado_num){
                 et5.setText("Felicitaciones");
+                imggan.setImageResource(R.drawable.correcto);
+                reload();
+            }
             else
+            {
+                imggan.setImageResource(R.drawable.error);
                 et5.setText("Vuelva a Intentarlo");
+            }
         }
         else if ( operacion[int_num3].equals("*")){
             resultado_num = Double.parseDouble(df.format(int_num1*int_num2));
-            if(result == resultado_num)
+            if(result == resultado_num){
                 et5.setText("Felicitaciones");
+                imggan.setImageResource(R.drawable.correcto);
+                reload();
+            }
             else
+            {
+                imggan.setImageResource(R.drawable.error);
                 et5.setText("Vuelva a Intentarlo");
+            }
         }
         else if ( operacion[int_num3].equals("/")){
             resultado_num = Double.parseDouble(df.format(int_num1/int_num2));
-            if(result == resultado_num)
+            if(result == resultado_num){
                 et5.setText("Felicitaciones");
+                imggan.setImageResource(R.drawable.correcto);
+                reload();
+            }
+
             else
+            {
+                imggan.setImageResource(R.drawable.error);
                 et5.setText("Vuelva a Intentarlo");
+            }
         }
     }
 
